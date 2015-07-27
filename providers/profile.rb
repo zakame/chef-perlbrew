f45fd73714eff1314ebb8ae2aec1ca6c4de5956e
J.R. Mash <jrmash@cpan.org>
2015-07-27 16:09:45 -0700

    Convert 'Profile' Recipe to LWRP
    Created the ‘profile’ lwrp and converted the recipe of the same name to
    use it.

--- providers/profile.rb
+++ providers/profile.rb
@@ -1,9 +1,9 @@
-# 
+#
 # Author:: David A. Golden
 # Contributors:: Jaryd Malbin <jaryd@duckduckgo.com>, J.R. Mash <jrmash@cpan.org>
 # Cookbook Name:: perlbrew
-# Recipe:: profile
-#                                                                                                                       
+# Provider:: perlbrew_profile
+#
 # Copyright:: 2012, David A. Golden <dagolden@cpan.org>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,7 +16,19 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License. 
+# limitations under the License.
 #
-perlbrew_profile "/etc/profile.d/perlbrew.sh"
+action :install do
+    template new_resource.name do 
+        group new_resource.group
+        owner new_resource.owner
+        mode new_resource.mode
+        source new_resource.source
+    end
+end
 
+action :uninstall do
+    file new_resource.name do
+        action :delete
+    end
+end
--- resources/profile.rb
+++ resources/profile.rb
@@ -1,9 +1,9 @@
-# 
+#
 # Author:: David A. Golden
 # Contributors:: Jaryd Malbin <jaryd@duckduckgo.com>, J.R. Mash <jrmash@cpan.org>
 # Cookbook Name:: perlbrew
-# Recipe:: profile
-#                                                                                                                       
+# Resource:: perlbrew_profile
+#
 # Copyright:: 2012, David A. Golden <dagolden@cpan.org>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,7 +16,12 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License. 
+# limitations under the License.
 #
-perlbrew_profile "/etc/profile.d/perlbrew.sh"
+actions :install, :uninstall
+default_action :install
 
+attribute :group,   :kind_of => String,             :default => 'root'
+attribute :owner,   :kind_of => String,             :default => 'root'
+attribute :mode,    :kind_of => [Integer, String],  :default => 0644
+attribute :source,  :kind_of => String,             :default => 'perlbrew.sh.erb'
