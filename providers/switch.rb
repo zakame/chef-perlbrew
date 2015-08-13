00b1517b2b925670078590d145bd7c4cd72fa9bd
J.R. Mash <jrmash@cpan.org>
2015-08-13 11:52:08 -0700

    Fixed 'Switch' LWRP
    This should have been part of the previous commit, but alas…

--- providers/switch.rb
+++ providers/switch.rb
@@ -21,10 +21,10 @@ action :off do
     bash "perlbrew switch-off" do
         code <<-EOC
             source #{new_resource.root}/etc/bashrc
-            perlbrew switch-off
+            perlbrew switch-offperl
         EOC
         environment ({'PERLBREW_HOME' => new_resource.root, 'PERLBREW_ROOT' => new_resource.root})
-        only_if { ::File.exists?(perlbrew_bin) }
+        only_if { ::File.exists?("#{new_resource.root}/bin/perlbrew") }
     end
     new_resource.updated_by_last_action(true)
 end
@@ -36,7 +36,7 @@ action :switch do
             perlbrew switch #{new_resource.version}
         EOC
         environment ({'PERLBREW_HOME' => new_resource.root, 'PERLBREW_ROOT' => new_resource.root})
-        only_if { ::File.exists?(perlbrew_bin) }
+        only_if { ::File.exists?("#{new_resource.root}/bin/perlbrew") }
     end
     new_resource.updated_by_last_action(true)
 end
