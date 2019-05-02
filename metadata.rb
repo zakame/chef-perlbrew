f5b3b547511a2f2650473696a227bd23cf30d34b
Zak B. Elep <zakame@zakame.net>
2019-05-02 15:35:33 +0800

    metadata.rb: Update maintainer info
    Almost forgot to change these!

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
 name              'perlbrew'
 license           'Apache-2.0'
-maintainer        'J.R. Mash'
-maintainer_email  'jrmash@cpan.org'
+maintainer        'Zak B. Elep'
+maintainer_email  'zakame@cpan.org'
 version           '0.4.0'
 chef_version      '>= 12.5'
 
 description       'Configures and Installs perlbrew'
-issues_url        'https://github.com/jrmash/perlbrew/issues'
-source_url        'https://github.com/jrmash/perlbrew'
+issues_url        'https://github.com/zakame/chef-perlbrew/issues'
+source_url        'https://github.com/zakame/chef-perlbrew'
 
 depends 'build-essential'
 
