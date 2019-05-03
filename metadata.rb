f6256726056d5b8bcffae84fd56e1efa37d276a9
Zak B. Elep <zakame@zakame.net>
2019-05-03 12:27:19 +0800

    metadata.rb: Bump version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              'perlbrew'
 license           'Apache-2.0'
 maintainer        'Zak B. Elep'
 maintainer_email  'zakame@cpan.org'
-version           '0.4.0'
+version           '0.4.1'
 chef_version      '>= 12.5'
 
 description       'Configures and Installs perlbrew'
