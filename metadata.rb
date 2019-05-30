2c65d9c53e945a61a229bc0161ee89fd538df31c
Zak B. Elep <zakame@zakame.net>
2019-05-30 14:53:14 +0800

    metadata.rb: Bump minimum required Chef version to 14
    Chef 15 foodcritic will now complain about this explicitly depending on
    build-essential now, so drop this dependency (as we don't need to change
    the recipe for this anyway.)
    
    https://github.com/Foodcritic/foodcritic/commit/be564eb5f1b2519725c4b009bb17ba60116a5c58

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,12 @@ license           'Apache-2.0'
 maintainer        'Zak B. Elep'
 maintainer_email  'zakame@cpan.org'
 version           '0.4.2'
-chef_version      '>= 12.5'
+chef_version      '>= 14'
 
 description       'Configures and Installs perlbrew'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url        'https://github.com/zakame/chef-perlbrew/issues'
 source_url        'https://github.com/zakame/chef-perlbrew'
-
-depends 'build-essential'
 
 [ 'debian', 'ubuntu', 'centos', 'amazon' ].each do |os|
   supports os
