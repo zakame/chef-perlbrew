77102c4bbad78e6413fba58f3f3cf76d89b5c04d
Zak B. Elep <zakame@zakame.net>
2019-11-15 19:12:18 +0800

    Update to use Perl 5.30.1

--- test/fixtures/cookbooks/test/recipes/custom_resource.rb
+++ test/fixtures/cookbooks/test/recipes/custom_resource.rb
@@ -6,29 +6,29 @@ perlbrew '/opt/perlbrew' do
   upgrade false
 end
 
-perlbrew_perl '5.30.0' do
+perlbrew_perl '5.30.1' do
   install_options '--noman --notest'
 end
 
 perlbrew_profile '/etc/profile.d/perlbrew.sh'
 
-perlbrew_lib '5.30.0@mylib'
+perlbrew_lib '5.30.1@mylib'
 
 perlbrew_cpanm 'install Mojolicious' do
   modules ['Mojolicious']
   options '--notest'
-  perlbrew '5.30.0@mylib'
+  perlbrew '5.30.1@mylib'
 end
 
-perlbrew_switch '5.30.0@mylib'
+perlbrew_switch '5.30.1@mylib'
 
 perlbrew_run 'Run "mojo version"' do
   command 'mojo version'
-  perlbrew '5.30.0@mylib'
+  perlbrew '5.30.1@mylib'
 end
 
 perlbrew_switch 'off'
 
-perlbrew_lib '5.30.0@mylib' do
+perlbrew_lib '5.30.1@mylib' do
   action :delete
 end
