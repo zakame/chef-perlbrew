60be7cc2eeee5a58c31ef9906f707bf1185ba440
Zak B. Elep <zakame@zakame.net>
2020-10-20 16:00:52 +0800

    :gear: Use Perl 5.32.0 for custom resource testing

--- test/fixtures/cookbooks/test/recipes/custom_resource.rb
+++ test/fixtures/cookbooks/test/recipes/custom_resource.rb
@@ -3,29 +3,29 @@ apt_update
 node.default['perlbrew']['self_upgrade'] = false
 include_recipe 'perlbrew::default'
 
-perlbrew_perl '5.30.1' do
+perlbrew_perl '5.32.0' do
   install_options '--noman --notest'
 end
 
 perlbrew_profile '/etc/profile.d/perlbrew.sh'
 
-perlbrew_lib '5.30.1@mylib'
+perlbrew_lib '5.32.0@mylib'
 
 perlbrew_cpanm 'install Mojolicious' do
   modules ['Mojolicious']
   options '--notest'
-  perlbrew '5.30.1@mylib'
+  perlbrew '5.32.0@mylib'
 end
 
-perlbrew_switch '5.30.1@mylib'
+perlbrew_switch '5.32.0@mylib'
 
 perlbrew_run 'Run "mojo version"' do
   command 'mojo version'
-  perlbrew '5.30.1@mylib'
+  perlbrew '5.32.0@mylib'
 end
 
 perlbrew_switch 'off'
 
-perlbrew_lib '5.30.1@mylib' do
+perlbrew_lib '5.32.0@mylib' do
   action :delete
 end
